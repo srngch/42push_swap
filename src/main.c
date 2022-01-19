fd0002a469fd399f94429a9d49975d298b046359
srngch <srngch.dev@gmail.com>
2022-01-19 21:36:42 +0900

    add: read arguments

--- src/main.c
+++ src/main.c
@@ -3,29 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sarchoi <sarchoi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 14:18:42 by sarchoi           #+#    #+#             */
-/*   Updated: 2022/01/15 17:13:34 by sarchoi          ###   ########.fr       */
+/*   Updated: 2022/01/19 21:24:11 by sarchoi          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push_swap(int argc, char *argv)
+t_stack *parse_arguments(int argc, char **argv)
 {
+	t_stack *stack;
+	char **  nums;
+	int      i;
+
+	stack = init_stack();
+	while (argc > 1)
+	{
+		if (ft_strchr(argv[argc - 1], ' '))
+		{
+			nums = ft_split(argv[argc - 1], ' ');
+			i = 0;
+			while (nums[i + 1])
+				i++;
+			while (i > -1)
+				stack_push(stack, ft_atoi(nums[i--]));
+		}
+		else
+		{
+			if (!stack_push(stack, ft_atoi(argv[argc - 1])))
+				return (NULL);
+		}
+		argc--;
+	}
+	// TODO: free nums
+	free(nums);
+	return (stack);
+}
+
+int main(int argc, char **argv)
+{
+	t_ps ps;
+
+	if (argc < 1)
+		return (EXIT_SUCCESS);
+	// TODO: parse argv
+	ps.a = parse_arguments(argc, argv);
+	print_stack(ps.a);
 	// TODO: check if argv is valid
 	// TODO: make a stack for each argument
 	// TODO: sort the stacks
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc == 1)
-	{
-		// TODO: error or not ?
-		return (EXIT_SUCCESS);
-	}
-	push_swap(argc, argv);
+	// TODO: print the result
+	// TODO: free the stacks
 	return (EXIT_SUCCESS);
 }
--- src/stack.c
+++ src/stack.c
@@ -6,7 +6,7 @@
 /*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 16:22:26 by sarchoi           #+#    #+#             */
-/*   Updated: 2022/01/19 15:52:02 by sarchoi          ###   ########seoul.kr  */
+/*   Updated: 2022/01/19 21:30:21 by sarchoi          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int	stack_pop(t_stack *stack)
 	if (!stack->top)
 		return (0);
 	item = stack->top;
-	data = *(item->content);
+	data = *(int *)(item->content);
 	stack->top = item->next;
 	free(item);
 	stack->size--;
@@ -45,11 +45,22 @@ int	stack_pop(t_stack *stack)
 int	stack_push(t_stack *stack, int data)
 {
 	t_list	*item;
+	int		*new_content;
 
 	item = (t_list *)malloc(sizeof(t_list));
 	if (!item)
 		return (FT_FALSE);
-	item->content = &data;
+	new_content = (int *)malloc(sizeof(int));
+	if (!new_content)
+		return (0);
+	*new_content = data;
+	item->content = new_content;
+	if (!stack->top)
+	{
+		stack->top = item;
+		stack->size++;
+		return (FT_TRUE);
+	}
 	item->next = stack->top;
 	stack->top = item;
 	stack->size++;
@@ -65,23 +76,24 @@ int	stack_is_empty(t_stack *stack)
 
 // make the stack
 // return: the stack, NULL if fail
-t_stack	*make_stack(t_stack *stack, int size, int **data)
-{
-	t_stack	*new_stack;
-	int		i;
+// t_stack	*make_stack(t_stack *stack, int size, int **data)
+// {
+// 	t_stack	*new_stack;
+// 	int		i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (!stack_push(stack, data[i]))
-		{
-			free_stack(stack);
-			return (NULL);
-		}
-		i++;
-	}
-	return (new_stack);
-}
+// 	i = 0;
+// 	while (i < size)
+// 	{
+// 		if (!stack_push(stack, *data[i]))
+// 		{
+// 			// TODO: free stack
+// 			// free_stack(stack);
+// 			return (NULL);
+// 		}
+// 		i++;
+// 	}
+// 	return (new_stack);
+// }
 
 // init the stack
 // return: the stack, NULL if fail
@@ -89,7 +101,24 @@ t_stack	*init_stack(void)
 {
 	t_stack	*stack;
 	
+	stack = (t_stack *)malloc(sizeof(t_stack));
+	if (!stack)
+		return (NULL);
 	stack->top = NULL;
 	stack->size = 0;
 	return (stack);
+}
+
+int	print_stack(t_stack *stack)
+{
+	t_list	*tmp;
+
+	tmp = stack->top;
+	while (tmp)
+	{
+		printf("%d ", *(int *)(tmp->content));
+		tmp = tmp->next;
+	}
+	printf("\n");
+	return (FT_TRUE);
 }
