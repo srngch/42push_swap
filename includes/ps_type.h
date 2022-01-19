661a578b99093966c5966cde19135633877fc46b
srngch <srngch.dev@gmail.com>
2022-01-19 16:49:58 +0900

    modify 42 header

--- includes/ps_type.h
+++ includes/ps_type.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ps_type.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sarchoi <sarchoi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 14:41:28 by sarchoi           #+#    #+#             */
-/*   Updated: 2022/01/15 16:21:59 by sarchoi          ###   ########.fr       */
+/*   Updated: 2022/01/19 15:53:23 by sarchoi          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,12 @@ typedef struct s_stack
 	t_list	*top;
 	int		size;
 }				t_stack;
+
+typedef struct s_ps
+{
+	t_stack	*a;
+	t_stack	*b;
+	int		size;
+}				t_ps;
 
 #endif
